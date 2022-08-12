f077730c4d639a8cbe2dac47c8bb4930eb81c4fe
Milsevka <lusindami99@gmail.com>
2022-08-12 21:58:15 +0300

    feat: add results

--- src/index.js
+++ src/index.js
@@ -38,9 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
-    decode
-}
+    let arr = [];
+    let str;
+    for (i = 0; i < expr.length; i += 10) {
+      str = [expr.substring(i, i + 10)];
+      arr.push(str);
+    }
+  
+    const symbols = [];
+  
+    for (let index = 0; index < arr.length; index++) {
+      const littleArray = arr[index];
+      const prevRes = [];
+  
+      for (let index = 0; index < littleArray.length; index++) {
+        let element = littleArray[index];
+  
+        for (let index = 0; index < element.length; index++) {
+          const number = element[index];
+  
+          if (number === "1") {
+            const binaryNumber = number + element[index + 1];
+  
+            if (binaryNumber === "10") {
+              prevRes.push(".");
+            } else if (binaryNumber === "11") {
+              prevRes.push("-");
+            }
+            index++;
+          }
+        }
+      }
+  
+      symbols.push(prevRes);
+    }
+  
+    const sumSymbols = [];
+  
+    symbols.map((element) => {
+      sumSymbols.push(element.join(""));
+    });
+  
+    let resultStr = "";
+  
+    sumSymbols.map((element) => {
+      resultStr += element ? MORSE_TABLE[element] : " ";
+    });
+  
+    return resultStr;
+  }
+  module.exports = {
+    decode,
+  };
